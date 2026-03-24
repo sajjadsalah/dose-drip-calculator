7cab13d86574f36420cbfdd62267104bdd20e1aa
sajjadsalah <64510841+sajjadsalah@users.noreply.github.com>
2026-03-24 03:30:07 +0300

    Add files via upload

---
+++ README_DEPLOY.txt
@@ -0,0 +1,21 @@
+Dose & Drip Calculator
+
+Final deployable files:
+- index.html
+- dose-drip-logo.svg
+- Dose-Drip-Calculator-final.zip
+
+How to upload:
+1. Open your hosting platform dashboard.
+2. Upload the ZIP file directly if the platform accepts ZIP uploads.
+3. If the platform expects files instead of a ZIP, extract the ZIP and upload index.html and dose-drip-logo.svg.
+
+Recommended static hosts:
+- Cloudflare Pages
+- Netlify Drop
+- GitHub Pages
+
+Notes:
+- This project is now prepared as a static site.
+- No Node.js server is required for deployment.
+- The app includes in-page review status indicators for reviewed, partially reviewed, and pending drug records.
---
+++ officialSourceCoverage_snippet.js
@@ -0,0 +1,218 @@
+const officialSourceCoverage = {
+    midazolam: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=d7362b4d-68a3-99a0-e053-2995a90a4f60"
+    ],
+    propofol: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=28d7ba00-f824-4e55-139a-03f509c099db"
+    ],
+    fentanyl: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=ce5db1c2-feb1-4ad2-847a-d02e865bd47e"
+    ],
+    ketamine: [
+        "https://dailymed.nlm.nih.gov/dailymed/search.cfm?query=ketamine%20injection"
+    ],
+    morphine: [
+        "https://dailymed.nlm.nih.gov/dailymed/search.cfm?query=morphine%20sulfate%20injection"
+    ],
+    dexmedetomidine: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=92b7ddaa-e45a-4b64-a802-bb5df8771dd6"
+    ],
+    rocuronium: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=309aeb1b-8022-4df7-8194-ae6529e3395c"
+    ],
+    cisatracurium: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=2cb05c7e-6228-4e2d-a270-7659e3db3fa7"
+    ],
+    succinylcholine: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=1e5891e2-e1e4-4587-b32c-b2e8082df9b6"
+    ],
+    neostigmine: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=7a9d996f-aecb-455f-95fc-1010e461d75d"
+    ],
+    sugammadex: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=37276948-ff0f-e0ab-e063-6394a90a0973"
+    ],
+    naloxone: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=4d66b96a-acbb-2292-e063-6294a90af788"
+    ],
+    atropine: [
+        "https://dailymed.nlm.nih.gov/dailymed/search.cfm?query=atropine%20sulfate%20injection"
+    ],
+    lignocaine: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=94155012-b1fc-430a-a1f5-30c21f4a3f80"
+    ],
+    diazepam: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=bfa85a63-967f-4176-a752-0bafabbaaae3"
+    ],
+    norepinephrine: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=ddbef60d-c20a-4ada-928f-e87074c08a54"
+    ],
+    epinephrine: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=60efd409-3555-4182-a68d-1cd7bc0d1bfc"
+    ],
+    dopamine: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=cb97d4a0-89ed-407c-a763-209386b6f75c"
+    ],
+    dobutamine: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=9943a961-3b50-4847-8a84-d8414ce38daa"
+    ],
+    phenylephrine: [
+        "https://dailymed.nlm.nih.gov/dailymed/search.cfm?query=phenylephrine%20injection"
+    ],
+    esmolol: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=3853a26d-782d-f4e7-e063-6294a90a6e47"
+    ],
+    amiodarone: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=e9151c1b-b70c-477b-868a-fffdc5c51830"
+    ],
+    magnesium_sulfate: [
+        "https://dailymed.nlm.nih.gov/dailymed/search.cfm?query=magnesium%20sulfate%20injection"
+    ],
+    dexamethasone: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=6ea9e6ab-0d33-4635-9e98-aa2fbd8921cc"
+    ],
+    ondansetron: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=e3f22b7e-b6ad-4799-93c7-d30540cada85"
+    ],
+    ketorolac: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=22de8e1c-5e68-fd78-e063-6394a90ad40b"
+    ],
+    paracetamol: [
+        "https://dailymed.nlm.nih.gov/dailymed/search.cfm?query=ofirmev"
+    ],
+    tranexamic: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=1553f7eb-d5a8-4274-9762-5bcc5ffcf809"
+    ],
+    remifentanil: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=8b4c8696-e23e-4c51-a4d2-babab5bd945a"
+    ],
+    hydromorphone: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=b325028e-0722-4c8c-9fdb-ab6fb0dc460c"
+    ],
+    sufentanil: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=20293943-46ff-4345-1aa4-929b4e017a25"
+    ],
+    alfentanil: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=bb56df52-5abf-47dc-ab8d-902cfe19ccb2"
+    ],
+    vecuronium: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=b012b1b1-9e59-4525-8f4d-dae98344d15f"
+    ],
+    atracurium: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=efe90ee2-fe9e-421f-9e54-2961085d3b12"
+    ],
+    pancuronium: [
+        "https://dailymed.nlm.nih.gov/dailymed/search.cfm?query=pancuronium%20bromide%20injection"
+    ],
+    flumazenil: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=3eefb01b-3d6a-a3eb-e063-6394a90a4829"
+    ],
+    glycopyrrolate: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=2972f22e-378c-450b-8869-1354a9bd0ffc"
+    ],
+    nitroglycerin: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=3f2c9570-a544-1982-e063-6294a90a3067"
+    ],
+    nitroprusside: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=a3b83d0e-b8f7-5e90-c363-1edfcd27a33d"
+    ],
+    labetalol: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=99a715a0-4579-410c-b102-7ef6fab8db55"
+    ],
+    diltiazem: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=734beeef-3968-4137-af2c-276e8a9317df"
+    ],
+    vasopressin: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=b1147beb-743e-4c62-8927-91192447f8b8"
+    ],
+    insulin_regular: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=b519bd83-038c-4ec5-a231-a51ec5cc291f"
+    ],
+    calcium_gluconate: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=16307c00-d0d5-4e05-821f-17b0c1a29fc9"
+    ],
+    potassium_chloride: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=aad6e18f-04f8-4799-b366-873ec5a50317"
+    ],
+    mannitol: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=430669d7-9ad1-9a04-e063-6394a90ababb"
+    ],
+    furosemide: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=1b9117d5-0dd9-4577-a1fd-bb0aaf7e68ec"
+    ],
+    clonidine: [
+        "https://dailymed.nlm.nih.gov/dailymed/search.cfm?query=clonidine%20injection"
+    ],
+    octreotide: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=387d7f8d-9762-4f6b-99e1-bdc50b8a1633"
+    ],
+    thiopental: [
+        "https://dailymed.nlm.nih.gov/dailymed/search.cfm?query=thiopental%20sodium%20for%20injection"
+    ],
+    lorazepam: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=5fc0e987-61c9-40c4-b0d5-fcea07c8733e"
+    ],
+    etomidate: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=9c2d0014-9e74-40b2-b452-255016c81cc1"
+    ],
+    ephedrine: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=38723cb3-dfca-4c81-8c6f-63dd99929191"
+    ],
+    nicardipine: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=b0e2bed3-f928-43ab-a05c-275bedc9856f"
+    ],
+    clevidipine: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=a6826aa3-fabb-4ff1-a7a3-cd4c34e3a330"
+    ],
+    hydralazine: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=f3c806a4-b971-421c-bcb9-9979c6d7aaec"
+    ],
+    metoprolol: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=1e787808-b3e0-417a-b333-80619d15432b"
+    ],
+    adenosine: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=48d72391-b3d4-bd97-e063-6294a90a9d6f"
+    ],
+    milrinone: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=0fdaee3c-2144-4ca7-9868-54d251f468c0"
+    ],
+    levetiracetam: [
+        "https://dailymed.nlm.nih.gov/dailymed/search.cfm?query=levetiracetam%20injection"
+    ],
+    fosphenytoin: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=a0def32c-e693-43e5-9509-0cef9642441c"
+    ],
+    bumetanide: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=b7cd724b-817b-4b45-8133-1ebf505867e7"
+    ],
+    phenytoin: [
+        "https://dailymed.nlm.nih.gov/dailymed/search.cfm?query=phenytoin%20sodium%20injection"
+    ],
+    procainamide: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=17e47845-daad-434c-a784-6d3875b0d704"
+    ],
+    methylene_blue: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=e2d9d96c-a711-47c5-855a-7eb7f163493e"
+    ],
+    phenobarbital: [
+        "https://dailymed.nlm.nih.gov/dailymed/search.cfm?query=phenobarbital%20sodium%20injection"
+    ],
+    methohexital: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=6fb09acf-4937-47a0-a9da-084ec062da6f"
+    ],
+    droperidol: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=147e033d-d997-4ef6-8bb5-a9ba372590b2"
+    ],
+    metoclopramide: [
+        "https://dailymed.nlm.nih.gov/dailymed/search.cfm?query=metoclopramide%20injection"
+    ],
+    dantrolene: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=4df35098-8702-46be-ac67-30cfdf1aa570"
+    ],
+    enalaprilat: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=5d398da1-020c-ccb1-a775-ce8e0b29d621"
+    ],
+    angiotensin_ii: [
+        "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?setid=727d1102-fc46-46f8-b78e-fd13f0738fe4"
+    ]
+};
